15f929245aa57c8e44bb065ea44ec6cfbb8fa5b3
legendtosif <toofanxd72@gmail.com>
2024-12-24 19:19:47 +0530

    Update couples.py

--- SONALI/plugins/tools/couples.py
+++ SONALI/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from SONALI.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="ꪜ 𝛊 ɭ ɭ ᧘ 𝛊 𝛈",
-            url=f"https://t.me/iamakki001",
+            text="˹ᴀɴᴏɴʏᴍᴏᴜs - xᴅ˼",
+            url=f"https://t.me/ll_Anonymous_Bots_ll",
         ),
     ],
 ]
